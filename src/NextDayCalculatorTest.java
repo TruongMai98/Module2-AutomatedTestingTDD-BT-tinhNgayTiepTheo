fc1040320fa50dabd20e41de2b46ccae565481f8
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 00:10:04 +0700

    add test case 31/1/2018

--- src/NextDayCalculatorTest.java
+++ src/NextDayCalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,19 @@ class NextDayCalculatorTest {
         int monthTest = 1;
         int yearTest = 2018;
         String excepted = "2/1/2018";
+
+        String result = NextDayCalculator.getNextDay(dateTest, monthTest, yearTest);
+        assertEquals(excepted, result);
+
+    }
+
+    @Test
+    @DisplayName("case 31/1/2018")
+    void testDay31Month1Year2018() {
+        int dateTest = 31;
+        int monthTest = 1;
+        int yearTest = 2018;
+        String excepted = "1/2/2018";
 
         String result = NextDayCalculator.getNextDay(dateTest, monthTest, yearTest);
         assertEquals(excepted, result);
