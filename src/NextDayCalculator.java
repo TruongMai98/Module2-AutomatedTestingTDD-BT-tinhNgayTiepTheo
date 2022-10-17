a5df668ee8bda022bd6d10ca795894c4aaee9290
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 00:28:16 +0700

    pass test case 30/4/2018

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -3,7 +3,25 @@ public class NextDayCalculator {
     public static final String CONNECTION = "/";
 
     public static String getNextDay(int day, int month, int year) {
-        int lastDayOfMonth = 31;
+        int lastDayOfMonth = 0;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                lastDayOfMonth = 31;
+                break;
+            case 2:
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                lastDayOfMonth = 30;
+                break;
+        }
         int firstDayOfMonth = 1;
         if (day == lastDayOfMonth) {
             day = firstDayOfMonth;
