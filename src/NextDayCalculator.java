9cb26a034f356355efc29212820a2334ccedd44c
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-18 00:12:10 +0700

    pass test case 31/1/2018

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -3,6 +3,12 @@ public class NextDayCalculator {
     public static final String CONNECTION = "/";
 
     public static String getNextDay(int day, int month, int year) {
-        return ++day + CONNECTION + month + CONNECTION + year;
+        if (day == 31) {
+            day = 1;
+            month++;
+        } else {
+            day++;
+        }
+        return day + CONNECTION + month + CONNECTION + year;
     }
 }
